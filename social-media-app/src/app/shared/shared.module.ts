17c452923ec8c62058372f68d573e1d0fedad132
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2020-02-27 19:06:44 +0200

    fix toDate method

--- social-media-app/src/app/shared/shared.module.ts
+++ social-media-app/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from "@angular/router";
 import { LoaderComponent } from "./loader/loader.component";
 import { PasswordMatchDirective } from "./directives/password-match.directive";
 import { SubstringPipe } from "./pipes/substring.pipe";
+import { ToDatePipe } from './pipes/toDate.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, PasswordMatchDirective, SubstringPipe],
+  declarations: [LoaderComponent, PasswordMatchDirective, SubstringPipe, ToDatePipe],
   imports: [CommonModule, RouterModule],
-  exports: [LoaderComponent, PasswordMatchDirective, SubstringPipe]
+  exports: [LoaderComponent, PasswordMatchDirective, SubstringPipe, ToDatePipe]
 })
 export class SharedModule {}
